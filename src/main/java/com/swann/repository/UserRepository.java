b640e4e62f2e08fa1d78241964231316bc665c82
Swann <princeapurbask@gmail.com>
2021-03-20 22:38:41 +0600

    User repository is defined

---
+++ src/main/java/com/swann/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.swann.repository;
+
+import com.swann.model.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+}
