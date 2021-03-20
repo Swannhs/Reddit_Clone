c025ee73a47d452c9525008e7f8b3436c7ddfc6f
Swann <princeapurbask@gmail.com>
2021-03-20 22:36:43 +0600

    User model is defined

--- src/main/java/com/swann/model/User.java
+++ src/main/java/com/swann/model/User.java
@@ -8,7 +8,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
-import java.time.Instant;
+import javax.validation.constraints.NotEmpty;
 
 @Data
 @Entity
@@ -17,16 +17,13 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @NotBlank(message = "User Name is required")
+    @NotBlank(message = "Username is required")
     private String username;
 
     @NotBlank(message = "Password is required")
     private String password;
 
     @Email(message = "Please provide a valid email")
-    @NotBlank(message = "Email is required")
+    @NotEmpty(message = "Email is required")
     private String email;
-
-    private Instant joined;
-    private boolean enabled;
 }
