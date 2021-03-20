e6de4b7d8b8e3225728501aeadaf769ee1daa240
Swann <princeapurbask@gmail.com>
2021-03-20 23:50:18 +0600

    Added basic spring security

---
+++ src/main/java/com/swann/config/SecurityConfig.java
@@ -0,0 +1,19 @@
+package com.swann.config;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+
+@EnableWebFluxSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/api/auth/**")
+                .permitAll()
+                .anyRequest()
+                .authenticated();
+    }
+}
